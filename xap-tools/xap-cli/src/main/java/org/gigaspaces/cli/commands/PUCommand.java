7bc95cf94738a161e4fe8d8b0929e88edce0ddc2
Yohana Khoury <yohana@yohanakhoury.com>
2018-03-11 16:59:09 +0200

    GS-13438 removed headers from subcommands

--- xap-tools/xap-cli/src/main/java/org/gigaspaces/cli/commands/PUCommand.java
+++ xap-tools/xap-cli/src/main/java/org/gigaspaces/cli/commands/PUCommand.java
@@ -7,7 +7,7 @@ import picocli.CommandLine.*;
 import java.util.Arrays;
 import java.util.Collection;
 
-@Command(name="pu", headerHeading = XapMainCommand.HEADER, header = "List of available commands for Processing Unit operations")
+@Command(name="pu", header = "List of available commands for Processing Unit operations")
 public class PUCommand extends CliCommand implements SubCommandContainer {
 
     @Override
