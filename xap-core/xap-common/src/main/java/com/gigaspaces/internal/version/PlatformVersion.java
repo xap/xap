724500ce0ad69a17b11a9e2064dd429fde7e3e52
Yohana Khoury <yohana@yohanakhoury.com>
2018-05-13 13:53:19 +0300

    GS-13503 bump version to 12.3.1-SNAPSHOT - applied missed changes

--- xap-core/xap-common/src/main/java/com/gigaspaces/internal/version/PlatformVersion.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/internal/version/PlatformVersion.java
@@ -38,9 +38,9 @@ public class PlatformVersion {
     private final String productHelpUrl;
 
     public PlatformVersion(Properties properties) {
-        version = properties.getProperty("xap.version", "12.3.0");
+        version = properties.getProperty("xap.version", "12.3.1");
         milestone = properties.getProperty("xap.milestone", "m1");
-        buildNumber = properties.getProperty("xap.build.number", "18900-10");
+        buildNumber = properties.getProperty("xap.build.number", "19200-10");
         revision = properties.getProperty("xap.git.sha", "unspecified");
 
         shortOfficialVersion = "XAP " + version + " " + milestone.toUpperCase();
