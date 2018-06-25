ca87f675110da805fd238aff2507f333e86ebde3
Niv Ingberg <niv@gigaspaces.com>
2018-06-25 10:07:53 +0300

    GS-13584 Refactored shutdown process to avoid jdk deadlock

--- xap-tools/xap-cli/src/main/java/org/gigaspaces/cli/commands/utils/ProcessBuilderWrapper.java
+++ xap-tools/xap-cli/src/main/java/org/gigaspaces/cli/commands/utils/ProcessBuilderWrapper.java
@@ -1,22 +1,41 @@
 package org.gigaspaces.cli.commands.utils;
 
+import java.io.IOException;
+
 /**
  * @since 12.3
  * @author evgeny
  */
 public class ProcessBuilderWrapper {
 
-  public ProcessBuilder getProcessBuilder() {
-    return processBuilder;
-  }
+    private final ProcessBuilder processBuilder;
+    private final Object lock = new Object();
+    private Process process;
+    private boolean destroyed;
 
-  private final ProcessBuilder processBuilder;
+    public ProcessBuilderWrapper(ProcessBuilder processBuilder ){
+        this.processBuilder = processBuilder;
+    }
 
-  public ProcessBuilderWrapper(ProcessBuilder processBuilder ){
-    this.processBuilder = processBuilder;
-  }
+    public Process start() throws IOException {
+        synchronized (lock) {
+            if (destroyed)
+                throw new IOException("Cannot create process - already destroyed");
+            this.process = processBuilder.start();
+            return process;
+        }
+    }
 
-  public boolean allowToStart(){
+    public Process destroy() {
+        synchronized (lock) {
+            destroyed = true;
+            if (process != null)
+                process.destroy();
+            return process;
+        }
+    }
+
+    public boolean allowToStart(){
     return true;
   }
 
