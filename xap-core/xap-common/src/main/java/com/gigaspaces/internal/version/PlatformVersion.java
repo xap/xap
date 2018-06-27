27c034196c420f3fb8ac496b9cc04c1e02655e1f
yaeln-pcu <yaeln@gigaspaces.com>
2018-06-27 11:58:13 +0300

    bumped version to 13.0.0

--- xap-core/xap-common/src/main/java/com/gigaspaces/internal/version/PlatformVersion.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/internal/version/PlatformVersion.java
@@ -38,9 +38,9 @@ public class PlatformVersion {
     private final String productHelpUrl;
 
     public PlatformVersion(Properties properties) {
-        version = properties.getProperty("xap.version", "12.3.1");
+        version = properties.getProperty("xap.version", "13.0.0");
         milestone = properties.getProperty("xap.milestone", "m1");
-        buildNumber = properties.getProperty("xap.build.number", "19200-10");
+        buildNumber = properties.getProperty("xap.build.number", "19901-10");
         revision = properties.getProperty("xap.git.sha", "unspecified");
 
         shortOfficialVersion = "XAP " + version + " " + milestone.toUpperCase();
