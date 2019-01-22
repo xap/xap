b751c39e2888506929da06fd3f3cb8fc54c0da70
yaelna <nahon.yael@gmail.com>
2019-01-22 12:41:12 +0200

    cleanup demo

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/lrmi/rdma/Client.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/lrmi/rdma/Client.java
@@ -22,8 +22,6 @@ package com.gigaspaces.lrmi.rdma;/*
 import com.ibm.disni.util.DiSNILogger;
 import org.apache.log4j.BasicConfigurator;
 
-import java.io.IOException;
-import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.concurrent.CompletableFuture;
@@ -31,67 +29,44 @@ import java.util.concurrent.CompletableFuture;
 
 public class Client {
 
-    private final GSRdmaEndpointFactory factory;
-    private final InetSocketAddress address;
-    private final GSRdmaClientEndpoint endpoint;
-    private String host = "192.168.33.137";
-    private int port = 8888;
+    private static String host = "192.168.33.137";
+    private static int port = 8888;
 
-    public Client() throws IOException {
-        factory = new GSRdmaEndpointFactory();
+    public static void main(String[] args) throws Exception {
+        BasicConfigurator.configure();
+
+        GSRdmaEndpointFactory factory = new GSRdmaEndpointFactory();
 
         //we have passed our own endpoint factory to the group, therefore new endpoints will be of type GSRdmaClientEndpoint
         //let's create a new client endpoint
-        endpoint = factory.create();
+        GSRdmaClientEndpoint endpoint = factory.create();
 
         //connect to the server
         InetAddress ipAddress = InetAddress.getByName(host);
-        address = new InetSocketAddress(ipAddress, port);
-    }
+        InetSocketAddress address = new InetSocketAddress(ipAddress, port);
 
-
-    public void run() throws Exception {
         try {
-            connect(1000);
-            DiSNILogger.getLogger().info("SimpleClient::client channel set up ");
+            endpoint.connect(address, 1000);
+            DiSNILogger.getLogger().info("::client channel set up ");
 
-            CompletableFuture<String> future = send("i am the client");
+            CompletableFuture<String> future = endpoint.getTransport().send("i am the client");
             String respond = future.get();
             DiSNILogger.getLogger().info(respond);
 
-            CompletableFuture<String> future1 = send("i am connected");
+            CompletableFuture<String> future1 = endpoint.getTransport().send("i am connected");
             respond = future1.get();
             DiSNILogger.getLogger().info(respond);
 
-            close();
+            endpoint.close();
+            DiSNILogger.getLogger().info("endpoint closed");
+            factory.close();
+            DiSNILogger.getLogger().info("group closed");
+
         } catch (Exception e) {
             DiSNILogger.getLogger().info("got exception", e);
         } finally {
             System.exit(0);
         }
-    }
-
-    private <T extends Serializable> CompletableFuture<T> send(Serializable msg) {
-        return endpoint.getTransport().send(msg);
-    }
-
-    private void connect(int timeout) throws Exception {
-        endpoint.connect(address, timeout);
-
-    }
-
-    private void close() throws IOException, InterruptedException {
-        //close everything
-        endpoint.close();
-        DiSNILogger.getLogger().info("endpoint closed");
-        factory.close();
-        DiSNILogger.getLogger().info("group closed");
-    }
-
-    public static void main(String[] args) throws Exception {
-        BasicConfigurator.configure();
-        Client simpleClient = new Client();
-        simpleClient.run();
     }
 
 }
