d688436961620ccac1f283ee0346410885bcc499
yaelna <nahon.yael@gmail.com>
2019-01-23 15:08:29 +0200

    mid refactoring

---
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/lrmi/rdma/LrmiRdmaMsg.java
@@ -0,0 +1,35 @@
+package com.gigaspaces.lrmi.rdma;
+
+import com.gigaspaces.lrmi.nio.ByteBufferPacketSerializer;
+import com.gigaspaces.lrmi.nio.IPacket;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class LrmiRdmaMsg extends RdmaMsg {
+
+    private IPacket payload;
+    private ByteBufferPacketSerializer serializer;
+
+    public LrmiRdmaMsg(IPacket payload, ByteBufferPacketSerializer serializer) {
+        this.payload = payload;
+        this.serializer = serializer;
+    }
+
+    @Override
+    public Object getPayload() {
+        return payload;
+    }
+
+    @Override
+    public void setPayload(Object payload) {
+        this.payload = (IPacket) payload;
+    }
+
+
+    //not using param ByteBuffer, using internal serializer buffer
+    @Override
+    public void serialize(ByteBuffer byteBuffer) throws IOException {
+        serializer.serialize(payload);
+    }
+}
