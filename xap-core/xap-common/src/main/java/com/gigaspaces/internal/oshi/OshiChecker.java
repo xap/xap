d1365573b58c91a205509d641a4e835373120d6c
Niv Ingberg <niv@gigaspaces.com>
2019-05-23 10:14:43 +0300

    GS-13813 - Log cleanup in OshiChecker Reviewer: Ayelet

--- xap-core/xap-common/src/main/java/com/gigaspaces/internal/oshi/OshiChecker.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/internal/oshi/OshiChecker.java
@@ -2,37 +2,37 @@ package com.gigaspaces.internal.oshi;
 
 import com.gigaspaces.CommonSystemProperties;
 import oshi.SystemInfo;
+
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class OshiChecker {
 
     private static final Logger logger = Logger.getLogger(OshiChecker.class.getName());
-    private static final boolean enabled = initEnabled();
-
     private static final SystemInfo systemInfo = initSystemInfo();
 
-    private static boolean initEnabled() {
-        String enabled = System.getProperty(CommonSystemProperties.OSHI_ENABLED, "");
-
-        if(enabled.isEmpty() || Boolean.parseBoolean(enabled)){
-            logger.fine("Oshi is enabled");
-            return true;
-        }
-        return false;
-    }
-
     private static SystemInfo initSystemInfo() {
-        try {
-            return new SystemInfo();
-        }catch (Throwable t) {
-            logger.warning("Oshi is not available "+t.getMessage());
+        String enabledProperty = System.getProperty(CommonSystemProperties.OSHI_ENABLED, "");
+        boolean enabled = enabledProperty.isEmpty() || Boolean.parseBoolean(enabledProperty);
+        if (!enabled) {
+            logger.info("Oshi is disabled");
+            return null;
         }
-        return null;
+
+        logger.log(enabledProperty.isEmpty() ? Level.FINE : Level.INFO, "Oshi is enabled");
+
+        try {
+            SystemInfo systemInfo = new SystemInfo();
+            logger.log(enabledProperty.isEmpty() ? Level.FINE : Level.INFO, "Oshi is available");
+            return systemInfo;
+        } catch (Throwable t) {
+            logger.log(enabledProperty.isEmpty() ? Level.FINE : Level.WARNING, "Oshi is not available: " + t.toString());
+            return null;
+        }
     }
 
     public static boolean isAvailable() {
-
-        return enabled && (systemInfo != null);
+        return systemInfo != null;
     }
 
     public static SystemInfo getSystemInfo(){
