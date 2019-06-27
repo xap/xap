ae821826f9949a974cb03c33f2d0675bcfd2e92a
mavigdor <moran@gigaspaces.com>
2019-06-27 12:33:43 +0300

    rephrase completion help message

--- xap-tools/xap-cli/src/main/java/org/gigaspaces/cli/commands/CompletionCommand.java
+++ xap-tools/xap-cli/src/main/java/org/gigaspaces/cli/commands/CompletionCommand.java
@@ -8,10 +8,11 @@ import java.io.File;
 import java.io.FileWriter;
 
 @CommandLine.Command(name="completion", header = {
-        "Generate completion script for bash/zsh shells",
-        "The generated script must be evaluated to provide interactive completion of gigaspaces commands.  This can be done by sourcing it from the .bash_profile.",
+        "Generate completion script for bash/zsh shells.",
         "",
-        "Detailed instructions on how to do this are available here:",
+        "The generated script must be evaluated to provide interactive completion of GigaSpaces CLI commands.",
+        "",
+        "For additional OS specific instructions and considerations refer to:",
         "https://docs.gigaspaces.com/14.5/admin/tools-cli.html#AutocompleteFunctionality",
         "",
         "Examples:",
@@ -20,12 +21,15 @@ import java.io.FileWriter;
         "",
         "  2. Alternatively, you can use the @|bold --target|@ option to create the autocompletion",
         "     code in a file and then source it:",
-        "   @|bold   $ ./gs.sh completion --target ~/gs.completion|@",
-        "   @|bold   $ source ~/gs.completion|@",
+        "   @|bold   $ ./gs.sh completion --target gs.completion|@",
+        "   @|bold   $ source gs.completion|@",
         "",
-        "  3. To permanently install the auto completion code, copy the generated script",
-        "     to the bash_completion.d folder:",
-        "   @|bold   $ ./gs.sh completion --target /path/to/bash_completion.d/gs.completion|@",
+        "     Source this script from the .bash_profile to auto-load on each session.",
+        "",
+        "  3. For completion to work automatically, bash shell completion behavior must be installed/supported.",
+        "     Copy the generated completion code to the bash_completion.d folder.",
+        "     All completion scripts under bash_completion.d folder are automatically sourced.",
+        "   @|bold   $ ./gs.sh completion --target <path to>/bash_completion.d/gs.completion|@",
         ""
 })
 public class CompletionCommand extends CliCommand {
