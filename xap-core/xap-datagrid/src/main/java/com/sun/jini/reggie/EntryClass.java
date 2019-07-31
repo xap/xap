38a27c89e14392ef6a887c4b3ca703df04be710b
mavigdor <moran@gigaspaces.com>
2019-07-31 15:14:45 +0300

    GS-13888 Use try-with-resources to close this "DataOutputStream"

--- xap-core/xap-datagrid/src/main/java/com/sun/jini/reggie/EntryClass.java
+++ xap-core/xap-datagrid/src/main/java/com/sun/jini/reggie/EntryClass.java
@@ -247,18 +247,19 @@ public class EntryClass implements Serializable {
         hash = 0;
         try {
             MessageDigest md = MessageDigest.getInstance("SHA");
-            DataOutputStream out = new DataOutputStream(
-                    new DigestOutputStream(new OptimizedByteArrayOutputStream(127), md));
-            if (superclass != null)
-                out.writeLong(superclass.hash);
-            out.writeUTF(name);
-            int startDeclaredFields = superclass != null ?
-                    superclass.numFields : 0;
-            for (int i = startDeclaredFields; i < fields.length; i++) {
-                out.writeUTF(fields[i].field.getName());
-                out.writeUTF(fields[i].field.getType().getName());
+            try (DataOutputStream out = new DataOutputStream(
+                    new DigestOutputStream(new OptimizedByteArrayOutputStream(127), md))) {
+                if (superclass != null)
+                    out.writeLong(superclass.hash);
+                out.writeUTF(name);
+                int startDeclaredFields = superclass != null ?
+                        superclass.numFields : 0;
+                for (int i = startDeclaredFields; i < fields.length; i++) {
+                    out.writeUTF(fields[i].field.getName());
+                    out.writeUTF(fields[i].field.getType().getName());
+                }
+                out.flush();
             }
-            out.flush();
             byte[] digest = md.digest();
             for (int i = Math.min(8, digest.length); --i >= 0; ) {
                 hash += ((long) (digest[i] & 0xFF)) << (i * 8);
