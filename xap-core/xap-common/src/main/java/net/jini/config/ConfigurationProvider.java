cfcd04f05245b7f970ad44270465dcdc6fb5b9a2
mavigdor <moran@gigaspaces.com>
2019-07-31 16:10:05 +0300

    GS-13888 Use try-with-resources to close this "BufferedReader"

--- xap-core/xap-common/src/main/java/net/jini/config/ConfigurationProvider.java
+++ xap-core/xap-common/src/main/java/net/jini/config/ConfigurationProvider.java
@@ -299,29 +299,28 @@ public class ConfigurationProvider {
      */
     private static String getProviderName(URL url)
             throws ConfigurationException, IOException {
-        InputStream in = null;
-        try {
-            in = url.openStream();
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(in, "utf-8"));
-            String result = null;
-            while (true) {
-                String line = reader.readLine();
-                if (line == null) {
-                    break;
-                }
-                int commentPos = line.indexOf('#');
-                if (commentPos >= 0) {
-                    line = line.substring(0, commentPos);
-                }
-                line = line.trim();
-                int len = line.length();
-                if (len != 0) {
-                    if (result != null) {
-                        throw new ConfigurationException(
-                                "resource specifies multiple providers");
+        try (InputStream in = url.openStream()) {
+            String result;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(in, "utf-8"))) {
+                result = null;
+                while (true) {
+                    String line = reader.readLine();
+                    if (line == null) {
+                        break;
                     }
-                    result = line;
+                    int commentPos = line.indexOf('#');
+                    if (commentPos >= 0) {
+                        line = line.substring(0, commentPos);
+                    }
+                    line = line.trim();
+                    int len = line.length();
+                    if (len != 0) {
+                        if (result != null) {
+                            throw new ConfigurationException(
+                                    "resource specifies multiple providers");
+                        }
+                        result = line;
+                    }
                 }
             }
             if (result == null) {
@@ -329,13 +328,6 @@ public class ConfigurationProvider {
                         "resource specifies no providers");
             }
             return result;
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                }
-            }
         }
     }
 }
