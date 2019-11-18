75ed7cfff430b9e6df2fdf3e148a15737b264e50
mavigdor <moran@gigaspaces.com>
2019-11-18 11:26:23 +0200

    improve readability of setenv-overrides

--- xap-core/xap-datagrid/bin/setenv-overrides.bat
+++ xap-core/xap-datagrid/bin/setenv-overrides.bat
@@ -1,12 +1,21 @@
 @echo off
-rem *******************************************************************************************************
-rem * Use this script to override default GigaSpaces settings using environment variables.                *
-rem * For more information see https://docs.gigaspaces.com/15.0/started/common-environment-variables.html *
-rem *******************************************************************************************************
+rem #*******************************************************************************************************
+rem # Use this script to override default GigaSpaces settings using environment variables.
+rem # For more information see https://docs.gigaspaces.com/15.0/started/common-environment-variables.html
+rem #
+rem # Common variables:
+rem # -----------------
+rem # JAVA_HOME          - The directory in which Java is installed
+rem # GS_OPTIONS_EXT     - Extra JVM options and system properties to append to java command line
+rem # GS_CLASSPATH_EXT   - Extra classpath to append to the predefined classpath
+rem # GS_NIC_ADDRESS     - The network interface card / host name/address to use (defaults to hostname)
+rem # GS_LOOKUP_GROUPS   - Lookup groups used for multicast discovery (defaults to version identifier)
+rem # GS_LOOKUP_LOCATORS - Lookup locators used for unicast discovery
+rem #*******************************************************************************************************
 
-rem set JAVA_HOME=                &rem The directory in which Java is installed
-rem set GS_OPTIONS_EXT=           &rem Extra java options and system properties to be used by GigaSpaces
-rem set GS_CLASSPATH_EXT=         &rem Extra classpath to be used by GigaSpaces
-rem set GS_NIC_ADDRESS=           &rem The network interface card / host name / host address that will be used by GigaSpaces (defaults to hostname)
-rem set GS_LOOKUP_GROUPS=         &rem Lookup groups used for multicast discovery (defaults to version identifier)
-rem set GS_LOOKUP_LOCATORS=       &rem Lookup locators used for unicast discovery
+rem set JAVA_HOME=
+rem set GS_OPTIONS_EXT=
+rem set GS_CLASSPATH_EXT=
+rem set GS_NIC_ADDRESS=
+rem set GS_LOOKUP_GROUPS=
+rem set GS_LOOKUP_LOCATORS=
--- xap-core/xap-datagrid/bin/setenv-overrides.sh
+++ xap-core/xap-datagrid/bin/setenv-overrides.sh
@@ -1,11 +1,20 @@
-## *******************************************************************************************************
-## * Use this script to override default GigaSpaces settings using environment variables.                *
-## * For more information see https://docs.gigaspaces.com/15.0/started/common-environment-variables.html *
-## *******************************************************************************************************
+##*******************************************************************************************************
+## Use this script to override default GigaSpaces settings using environment variables.
+## For more information see https://docs.gigaspaces.com/15.0/started/common-environment-variables.html
+##
+## Common variables:
+## -----------------
+## JAVA_HOME          - The directory in which Java is installed
+## GS_OPTIONS_EXT     - Extra JVM options and system properties to append to java command line
+## GS_CLASSPATH_EXT   - Extra classpath to append to the predefined classpath
+## GS_NIC_ADDRESS     - The network interface card / host name/address to use (defaults to hostname)
+## GS_LOOKUP_GROUPS   - Lookup groups used for multicast discovery (defaults to version identifier)
+## GS_LOOKUP_LOCATORS - Lookup locators used for unicast discovery
+##*******************************************************************************************************
 
-# export JAVA_HOME=               # The directory in which Java is installed
-# export GS_OPTIONS_EXT=          # Extra java options and system properties to be used by GigaSpaces
-# export GS_CLASSPATH_EXT=        # Extra classpath to be used by GigaSpaces
-# export GS_NIC_ADDRESS=          # The network interface card / host name / host address that will be used by GigaSpaces (defaults to hostname)
-# export GS_LOOKUP_GROUPS=        # Lookup groups used for multicast discovery (defaults to version identifier)
-# export GS_LOOKUP_LOCATORS=      # Lookup locators used for unicast discovery
+# export JAVA_HOME=
+# export GS_OPTIONS_EXT=
+# export GS_CLASSPATH_EXT=
+# export GS_NIC_ADDRESS=
+# export GS_LOOKUP_GROUPS=
+# export GS_LOOKUP_LOCATORS=
