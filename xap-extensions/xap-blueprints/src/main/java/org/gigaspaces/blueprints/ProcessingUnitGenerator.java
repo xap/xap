042e6703c7a5dd955f45cc8759bbbb1a39ebfab6
ayelet <ayelet.morris@gigaspaces.com>
2020-01-13 15:19:03 +0200

    try zip directory

--- xap-extensions/xap-blueprints/src/main/java/org/gigaspaces/blueprints/ProcessingUnitGenerator.java
+++ xap-extensions/xap-blueprints/src/main/java/org/gigaspaces/blueprints/ProcessingUnitGenerator.java
@@ -4,14 +4,10 @@ import com.gigaspaces.start.SystemLocations;
 import org.jini.rio.boot.PUZipUtils;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 import java.util.logging.Logger;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import static org.gigaspaces.blueprints.BlueprintUtils.getDefaultTarget;
 
@@ -28,21 +24,14 @@ public class ProcessingUnitGenerator {
         logger.info("************************* Target Path: "+target.toString());
         logger.info("************************* workDir Path: "+workDir.toString());
 
-        blueprint.generate(target, updatedProperties(blueprint.getValues(),properties));
-        File zipFile = new File(target.toString()+".zip");
+        blueprint.generate(target, properties);
+        File zipFile = new File(workDir.toString()+"/"+target.getFileName()+".zip");
+        if(zipFile.exists()) {
+            zipFile.delete();
+        }
         PUZipUtils.zip(tempDirectory.toFile(), zipFile);
 
-        logger.info("************************* zipFile exist: "+zipFile.exists());
-    }
+        logger.info("************************* zipFile exist: "+zipFile.exists()+ " ***  path: "+zipFile.toString());
 
-    private static Map<String, String> updatedProperties(Map<String, String> defaultProperties, Map<String, String> properties) {
-
-        defaultProperties.forEach((k, v) -> {
-            if (!properties.containsKey(k)) {
-                properties.put(k, defaultProperties.get(k));
-            }
-        });
-
-        return properties;
     }
 }
