a0cf4206d6a8d85b66da231c02f7bfd6696d6944
ayelet <ayelet.morris@gigaspaces.com>
2020-01-22 13:00:32 +0200

    GS-13992 - add bash scripts to blueprint stateful-with-db

--- xap-extensions/xap-blueprints/blueprints/stateful-with-db/templates/demo-start.sh
+++ xap-extensions/xap-blueprints/blueprints/stateful-with-db/templates/demo-start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-./{{project.artifactId}}-env.sh
+. ./{{project.artifactId}}-env.sh
 if [ ! -e target ]; then
     echo "Building processing units..."
     ./build.sh
@@ -10,8 +10,9 @@ xterm -e ./demo-db/run.sh &
 
 echo "Creating container for mirror processing unit..."
 ../gs.sh container create --zone={{project.artifactId}}-mirror --memory={{resources.mirror.memory}} localhost
-echo "Creating ${SPACE_INSTANCES} containers for space processing unit..."
-for ((i=0; i < "${SPACE_INSTANCES}"; i++)); do
+echo "Creating $SPACE_INSTANCES containers for space processing unit..."
+for (( c=1; c<=$SPACE_INSTANCES; c++ ))
+do
     ../gs.sh container create --zone={{project.artifactId}}-space --memory={{resources.space.memory}} localhost
 done
 
