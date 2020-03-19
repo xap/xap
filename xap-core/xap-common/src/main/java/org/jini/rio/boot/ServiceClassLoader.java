402c8dd8824305e3b50350847b8ca01e26ad01e5
yaelna <nahon.yael@gmail.com>
2020-03-19 18:52:31 +0200

    removed unnecessary filter of lo4j and slf4j jars in insightedge class loading

--- xap-core/xap-common/src/main/java/org/jini/rio/boot/ServiceClassLoader.java
+++ xap-core/xap-common/src/main/java/org/jini/rio/boot/ServiceClassLoader.java
@@ -5,9 +5,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,6 +28,7 @@ import java.util.*;
 import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * The ServiceClassLoader overrides getURLs(), ensuring all classes that need to be annotated with
@@ -276,8 +277,9 @@ public class ServiceClassLoader extends CustomURLClassLoader implements ClassAnn
 
     protected boolean isSystemClass(String name) {
         // Special case to ensure GigaSpaces-custom-jul-slf4j bridge is preferred.
-        if (name.equals("org.slf4j.bridge.SLF4JBridgeHandler"))
+        if (name.equals("org.slf4j.bridge.SLF4JBridgeHandler")) {
             return true;
+        }
         return !matches(name, systemClassExcludes);
     }
 
