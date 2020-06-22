4761109fd22460219d592e123c5cfbfbbbf9e92e
Niv Ingberg <niv@gigaspaces.com>
2020-06-22 17:24:10 +0300

    GS-14128 added missing Remote service

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/admin/manager/ManagerClusterInfoProvider.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/admin/manager/ManagerClusterInfoProvider.java
@@ -2,10 +2,13 @@ package com.gigaspaces.admin.manager;
 
 import com.gigaspaces.admin.ManagerClusterInfo;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
  * @author Niv Ingberg
  * @since 15.5
  */
-public interface ManagerClusterInfoProvider {
-    ManagerClusterInfo getManagerClusterInfo();
+public interface ManagerClusterInfoProvider extends Remote {
+    ManagerClusterInfo getManagerClusterInfo() throws RemoteException;;
 }
