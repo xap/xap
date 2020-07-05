b24cdd4002f7e3a5b346cf4c87878c964cb6cc38
Niv Ingberg <niv@gigaspaces.com>
2020-07-05 17:41:41 +0300

    GS-14128 renamed lock.release to close from closeable

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/attribute_store/SharedLock.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/attribute_store/SharedLock.java
@@ -1,10 +1,10 @@
 package com.gigaspaces.attribute_store;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-public interface SharedLock {
+public interface SharedLock extends Closeable {
     void acquire(long time, TimeUnit unit) throws IOException, TimeoutException;
-    void release() throws IOException;
 }
