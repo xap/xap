88f0c413b05a979d66d48901317b98acf831fb6a
Niv Ingberg <niv@gigaspaces.com>
2020-07-05 17:36:33 +0300

    GS-14128 modified acquire to throw timeout exception

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/attribute_store/SharedLock.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/attribute_store/SharedLock.java
@@ -1,8 +1,10 @@
 package com.gigaspaces.attribute_store;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public interface SharedLock {
-    boolean	acquire(long time, TimeUnit unit) throws Exception;
-    void release() throws Exception;
+    void acquire(long time, TimeUnit unit) throws IOException, TimeoutException;
+    void release() throws IOException;
 }
