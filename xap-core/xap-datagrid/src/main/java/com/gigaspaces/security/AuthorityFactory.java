c47e6904d5e3e854a61b5773373ea0e0ac7a64d4
mavigdor <moran@gigaspaces.com>
2020-07-06 18:20:35 +0300

    GS-14161 Add role-based user details provider
    Out-of-the-box experience when security is enabled:
    1. Default security.properties with Spring security-config.xml
    
    2. Config file declares 3 customizable roles:
       (ROLE_ADMIN, ROLE_DEVELOPER, ROLE_ANALYST)
    
    3. Short syntax for security authorities (privileges)
    
    4. loads default config/security/security.properties file,
       only if security-properties file was not set using sys prop.

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/security/AuthorityFactory.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/security/AuthorityFactory.java
@@ -17,16 +17,11 @@
 
 package com.gigaspaces.security;
 
-import com.gigaspaces.security.authorities.Constants;
-import com.gigaspaces.security.authorities.GridAuthority;
+import com.gigaspaces.security.authorities.*;
 import com.gigaspaces.security.authorities.GridAuthority.GridPrivilege;
-import com.gigaspaces.security.authorities.MonitorAuthority;
 import com.gigaspaces.security.authorities.MonitorAuthority.MonitorPrivilege;
-import com.gigaspaces.security.authorities.RoleAuthority;
 import com.gigaspaces.security.authorities.RoleAuthority.RolePrivilege;
-import com.gigaspaces.security.authorities.SpaceAuthority;
 import com.gigaspaces.security.authorities.SpaceAuthority.SpacePrivilege;
-import com.gigaspaces.security.authorities.SystemAuthority;
 import com.gigaspaces.security.authorities.SystemAuthority.SystemPrivilege;
 
 /**
@@ -44,8 +39,48 @@ public final class AuthorityFactory {
      *
      * @param authority An authority String representation.
      * @return An authority instance.
+     * @deprecated since 15.5 - use {@link #valueOf(String)} instead.
      */
     public static Authority create(String authority) {
+        return valueOf(authority);
+    }
+
+    /**
+     * Creates an {@link Authority} instance out its short (or long) name String representation.
+     * Calls {@link #create(String)} if short representation is not matched.
+     * @param authority An authority String representation
+     * @return An authority instance
+     * @since 15.5
+     */
+    public static Authority valueOf(String authority) {
+        switch (authority) {
+            case "MANAGE_GRID":  return valueOf(GridPrivilege.MANAGE_GRID);
+            case "MANAGE_PU":    return valueOf(GridPrivilege.MANAGE_PU);
+            case "PROVISION_PU": return valueOf(GridPrivilege.PROVISION_PU);
+            case "MONITOR_JVM":  return valueOf(MonitorPrivilege.MONITOR_JVM);
+            case "MONITOR_PU":   return valueOf(MonitorPrivilege.MONITOR_PU);
+            case "SPACE_ALTER":  return valueOf(SpacePrivilege.ALTER);
+            case "SPACE_CREATE":   return valueOf(SpacePrivilege.CREATE);
+            case "SPACE_EXECUTE":   return valueOf(SpacePrivilege.EXECUTE);
+            case "SPACE_READ":   return valueOf(SpacePrivilege.READ);
+            case "SPACE_WRITE":   return valueOf(SpacePrivilege.WRITE);
+            case "SPACE_TAKE":   return valueOf(SpacePrivilege.TAKE);
+        }
+        if (authority.startsWith("SPACE_")) {
+            return _valueOfWithClassPrefix("SpacePrivilege " + authority.substring("SPACE_".length()));
+        }
+        return _valueOfWithClassPrefix(authority);
+    }
+
+    /**
+     * Internal API - backwards full syntax support including privilege class name.
+     * Used to be the implementation of {@link #create(String)}.
+     *
+     * @param authority the authority String with Class Prefix
+     * @return an authority instance
+     * @since 15.5
+     */
+    private static Authority _valueOfWithClassPrefix(String authority) {
         String[] split = authority.split(Constants.DELIM);
         if (split.length < 2) {
             throw new IllegalArgumentException("Illegal Authority format: " + authority);
@@ -64,5 +99,25 @@ public final class AuthorityFactory {
         }
 
         throw new IllegalArgumentException("Unknown authority type; Could not create an Authority from: " + authority);
+    }
+
+    /**
+     * Converts Privilege to Authority
+     * @param privilege
+     * @return an Authority encapsulating this privilege
+     * @since 15.5
+     */
+    public static Authority valueOf(Privilege privilege) {
+        if (privilege instanceof GridPrivilege) {
+            return new GridAuthority((GridPrivilege) privilege);
+        } else if (privilege instanceof MonitorPrivilege) {
+            return new MonitorAuthority(((MonitorPrivilege) privilege));
+        } else if (privilege instanceof SpacePrivilege) {
+            return new SpaceAuthority(((SpacePrivilege) privilege));
+        } else if (privilege instanceof SystemPrivilege) {
+            return new SystemAuthority((SystemPrivilege) privilege);
+        }
+
+        throw new IllegalArgumentException("unknown privilege " + privilege);
     }
 }
