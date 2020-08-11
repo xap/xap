d077c80617fa768507c916643b78c90b429ecad6
Niv Ingberg <niv@gigaspaces.com>
2020-08-11 11:29:18 +0300

    GS-14202 fixed cluster info injection in blueprint

--- xap-extensions/xap-blueprints/blueprints/stateful-with-db/templates/{{project.artifactId}}-space/src/main/java/{{project.package-path}}/CustomSpaceConfig.java
+++ xap-extensions/xap-blueprints/blueprints/stateful-with-db/templates/{{project.artifactId}}-space/src/main/java/{{project.package-path}}/CustomSpaceConfig.java
@@ -3,16 +3,21 @@ package {{project.groupId}};
 import java.util.Properties;
 import javax.sql.DataSource;
 
-import org.openspaces.core.space.EmbeddedSpaceFactoryBean;
+import org.hibernate.SessionFactory;
+import org.openspaces.core.cluster.ClusterInfo;
+import org.openspaces.core.cluster.ClusterInfoContext;
 import org.openspaces.core.config.annotation.EmbeddedSpaceBeansConfig;
+import org.openspaces.core.space.EmbeddedSpaceFactoryBean;
 import org.openspaces.persistency.hibernate.DefaultHibernateSpaceDataSourceConfigurer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
-import org.hibernate.SessionFactory;
 
 public class CustomSpaceConfig extends EmbeddedSpaceBeansConfig {
-	
+
+	@ClusterInfoContext
+	private ClusterInfo clusterInfo;
+
 	@Value("${db.driver}")
 	private String dbDriver;
 	@Value("${db.url}")
@@ -34,8 +39,9 @@ public class CustomSpaceConfig extends EmbeddedSpaceBeansConfig {
 		factoryBean.setMirrored(true);
         factoryBean.setSpaceDataSource(new DefaultHibernateSpaceDataSourceConfigurer()
 		    .sessionFactory(initSessionFactory())
+		    .clusterInfo(clusterInfo)
 		    .limitResults(limitResults)
-            .create());
+		    .create());
 
 		Properties properties = new Properties();
         properties.setProperty("space-config.engine.cache_policy", "1"); // 1 == ALL IN CACHE
