1cbde445862ef41e70b5ae7c933acae0ebca99dc
mavigdor <moran@gigaspaces.com>
2020-09-21 17:40:07 +0300

    GS-14265 change property

--- xap-core/xap-common/src/main/java/com/gigaspaces/internal/os/ProcessCpuSampler.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/internal/os/ProcessCpuSampler.java
@@ -18,7 +18,7 @@ public interface ProcessCpuSampler {
     long sampleTotalCpuTime();
 
     /**
-     * Gets CPU usage of this process as a fraction of 100%.
+     * Gets CPU utilization of this process as a fraction of 100%.
      * <p>
      * This calculation sums CPU ticks across all processors and may exceed 100% for
      * multi-threaded processes. This is consistent with the cumulative CPU
@@ -34,22 +34,25 @@ public interface ProcessCpuSampler {
                 : NA;
     }
 
-    int cores = GsEnv.propertyInt("com.gs.metric.process.cpu.cores")
-            .get(Runtime.getRuntime().availableProcessors());
+    /**
+     * The CPU utilization is divided by the number of cores. To disable this default behavior, set
+     * System property <code>com.gs.process.cpu.utilization.divideByCores</code> to false.
+     */
+    int cores = GsEnv.propertyBoolean("com.gs.process.cpu.utilization.divideByCores").get(true)
+            ? Runtime.getRuntime().availableProcessors() : 1;
 
     /**
-     * The CPU process usage is divided by the number of cores to get a fraction of 100%.
-     * This is the default behavior.
+     * GigaSpaces expresses %CPU within a range from 0 to 100 on all machines even though some vendors
+     * scale to N*100% if multiple CPUs (N CPUs) exist.
      * <br><br>
-     * In Windows the CPU usage counts all cores and hyperthreading as a fraction of 100%,
-     * and that results in a value less than a 100% for a single process.
+     * In Windows, the CPU utilization is defined as the fraction of time that the process
+     * spends in kernel + user during the last sampling interval.
      * <br><br>
-     * In Linux/CentOS/MacOS, each thread can consume 100% of a core or hyperthread,
-     * and CPU usage of multiple threads per process results in a value of more than 100%.
-     * To get this behavior, set 'com.gs.metric.process.cpu.cores' to 1.
+     * In Unix/MacOs, the CPU utilization is the same as "top" divided by the number of CPUs
+     * (top shows utilization of a single CPU). It is the average utilization over the last sample period.
      * <br><br>
      *
-     * @return The number of available cores to consider when dividing the CPU usage by.
+     * @return The number of available cores to consider when dividing the CPU utilization by.
      */
     default int numberOfCores() {
         return cores;
