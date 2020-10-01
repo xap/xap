8d413a9664a95d26e1a93724f797a16c82f8370f
Barak Bar Orion <barak.bar@gmail.com>
2020-10-01 12:31:04 +0300

    Add the second test

--- xap-core/xap-datagrid/src/test/java/com/j_spaces/jdbc/parser/grammar/SqlParserTest.java
+++ xap-core/xap-datagrid/src/test/java/com/j_spaces/jdbc/parser/grammar/SqlParserTest.java
@@ -14,8 +14,14 @@ import java.util.stream.Collectors;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SqlParserTest {
-    private static Logger logger = LoggerFactory.getLogger(SqlParserTest.class);
-    private final List<String> inputs = Arrays.asList("SELECT REGION,ID_BB_PARSEKEY,EXCH_CODE,NAME,CRNCY,FLEX_ID,ID_BB_GLOBAL,ID_SEDOL1,ID_SEDOL2,SECURITY_TYP,EQY_SH_OUT,ID_BB_GLOBAL_COMPANY FROM bloomberg.bloomberg_tzero_out WHERE REGION = ? AND DDATE = ?");
+    private static final Logger logger = LoggerFactory.getLogger(SqlParserTest.class);
+    private final List<String> inputs = Arrays.asList("SELECT REGION,ID_BB_PARSEKEY,EXCH_CODE,NAME,CRNCY,FLEX_ID,ID_BB_GLOBAL,ID_SEDOL1,ID_SEDOL2,SECURITY_TYP,EQY_SH_OUT,ID_BB_GLOBAL_COMPANY FROM bloomberg.bloomberg_tzero_out WHERE REGION = ? AND DDATE = ?"
+            , "SELECT TICKER, LAST_PRICE, DBTIMESTAMP\n" +
+                    "FROM BPIPE_RT.REAL_TIME t1\n" +
+                    "WHERE TICKER LIKE '%Curncy%'\n" +
+                    "and DATA_RECEIVED_TIMESTAMP = (SELECT MAX(DATA_RECEIVED_TIMESTAMP) FROM BPIPE_RT.REAL_TIME t2 WHERE t1.TICKER=t2.TICKER and LAST_PRICE is not null)\n" +
+                    "ORDER BY DATA_RECEIVED_TIMESTAMP DESC;"
+    );
 
     public SqlParserTest() {
         logger.info("creating SqlParserTest");
