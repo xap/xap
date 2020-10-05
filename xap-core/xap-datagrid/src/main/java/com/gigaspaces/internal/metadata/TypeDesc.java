9b629649ee3e35132e654dd96a5ec2ff287edf2a
yaelna <nahon.yael@gmail.com>
2020-10-05 17:24:50 +0300

    revert on by default

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/metadata/TypeDesc.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/metadata/TypeDesc.java
@@ -19,7 +19,6 @@ package com.gigaspaces.internal.metadata;
 import com.gigaspaces.annotation.pojo.FifoSupport;
 import com.gigaspaces.client.storage_adapters.class_storage_adapters.ClassBinaryStorageAdapter;
 import com.gigaspaces.client.storage_adapters.class_storage_adapters.ClassBinaryStorageAdapterRegistry;
-import com.gigaspaces.client.storage_adapters.class_storage_adapters.SerializedAdapter;
 import com.gigaspaces.document.SpaceDocument;
 import com.gigaspaces.internal.io.CustomClassLoaderObjectInputStream;
 import com.gigaspaces.internal.io.IOUtils;
@@ -81,7 +80,7 @@ public class TypeDesc implements ITypeDesc {
     private ITypeIntrospector<?> _objectIntrospector;
     private Map<String, SpaceIndex> _indexes;
     private TypeQueryExtensions queryExtensionsInfo;
-    private ClassBinaryStorageAdapter classStorageAdapter = ClassBinaryStorageAdapterRegistry.getInstance().getOrCreate(SerializedAdapter.class);
+    private ClassBinaryStorageAdapter classStorageAdapter;
 
     private int _sequenceNumberFixedPropertyPos;  //-1  if none
 
