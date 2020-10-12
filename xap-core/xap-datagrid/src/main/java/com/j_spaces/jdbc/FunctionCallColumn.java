0d832dc3ea0899b700e70f110f6e473975e0fc14
alonshoham <alon.shoham@gmail.com>
2020-10-12 10:31:44 +0300

    Enabled aggregation + function call in select clause, added tests

--- xap-core/xap-datagrid/src/main/java/com/j_spaces/jdbc/FunctionCallColumn.java
+++ xap-core/xap-datagrid/src/main/java/com/j_spaces/jdbc/FunctionCallColumn.java
@@ -3,15 +3,16 @@ package com.j_spaces.jdbc;
 
 import com.gigaspaces.internal.transport.IEntryPacket;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.function.Function;
 
 @com.gigaspaces.api.InternalApi
-public class FunctionCallColumn extends SelectColumn {
+public class FunctionCallColumn<R extends Serializable, T extends Serializable> extends SelectColumn {
 
     private List params;
 
-    private Function<Object, Object> f;
+    private Function<R, T> f;
 
     public FunctionCallColumn() {
     }
@@ -23,7 +24,7 @@ public class FunctionCallColumn extends SelectColumn {
         this.params.remove(0); // removes first param which is always the column name
 
         if (functionName.equals("REPLACE")) {
-            f = (obj) -> obj.toString().replace(params.get(0).toString(), params.get(1).toString());
+            f = (obj) -> (T) obj.toString().replace(params.get(0).toString(), params.get(1).toString());
         } else {
             throw new RuntimeException("Unknown function [" + functionName + "]");
         }
@@ -31,9 +32,11 @@ public class FunctionCallColumn extends SelectColumn {
 
     @Override
     public Object getFieldValue(IEntryPacket entry) {
-        Object fValue = super.getFieldValue(entry);
+        R fValue = (R) super.getFieldValue(entry);
         return f.apply(fValue);
     }
 
-
+    public Function<R, T> getF() {
+        return f;
+    }
 }
