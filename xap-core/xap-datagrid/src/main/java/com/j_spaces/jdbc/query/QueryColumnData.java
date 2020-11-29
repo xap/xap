785a26572e5c4f75aba11edf88fce423a81d5408
alonshoham <alon.shoham@gmail.com>
2020-11-29 12:57:57 +0200

    Search for matching columns recursively in table data

--- xap-core/xap-datagrid/src/main/java/com/j_spaces/jdbc/query/QueryColumnData.java
+++ xap-core/xap-datagrid/src/main/java/com/j_spaces/jdbc/query/QueryColumnData.java
@@ -211,6 +211,14 @@ public class QueryColumnData implements Serializable {
                 if (prefix != null && columnPath.startsWith(prefix + "."))
                     return new QueryColumnData(tableData, columnPath.substring(prefix.length() + 1));
             }
+            if(tableData.getSubQuery() != null && tableData.getSubQuery() instanceof  AbstractDMLQuery) {
+                try {
+                    return findUnique(((AbstractDMLQuery) tableData.getSubQuery()).getTablesData(), t -> tryInitWithPrefix(t, columnPath), (r1, r2) -> ambigFormatter(columnPath, r1, r2));
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                    return null;
+                }
+            }
         }
         return null;
     }
