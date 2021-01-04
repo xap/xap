c023629bb41d24008ce4d6e641e0a73b07ddb5aa
Efrat Kimchi <efrat.kimchi@gigaspaces.com>
2021-01-04 18:37:04 +0200

    added prints

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/SpaceImpl.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/SpaceImpl.java
@@ -1534,8 +1534,8 @@ public class SpaceImpl extends AbstractService implements IRemoteSpace, IInterna
                 changeSpaceState(ISpaceState.STARTED, true, true);
                 _logger.info("++++++++++++++++++++details: " + _puName + "  " +  _instanceId + "++++++++++++++++");
                 if(_instanceId.contains("2_1")){
-                    _logger.info("++++++++++++++++++++++++ in sleep+++++++++++++++++");
-                    Thread.sleep(1000 * 60 );
+                   // _logger.info("++++++++++++++++++++++++ in sleep+++++++++++++++++");
+                   /// Thread.sleep(1000 * 60 );
                 }
 
                 _internalSpaceModesListeners.afterSpaceModeChange(spaceMode);
