3531871e16df03a9b8a27a55199b2e96c407b88f
yaelna <nahon.yael@gmail.com>
2021-01-05 16:58:35 +0200

    GS-14309 on by default

--- xap-core/xap-datagrid/src/test/java/com/gigaspaces/metadata/SpaceTypeDescriptorTestCase.java
+++ xap-core/xap-datagrid/src/test/java/com/gigaspaces/metadata/SpaceTypeDescriptorTestCase.java
@@ -133,6 +133,11 @@ import java.util.Set;
 
 @com.gigaspaces.api.InternalApi
 public class SpaceTypeDescriptorTestCase extends TestCase {
+    @Override
+    protected void setUp() throws Exception {
+        System.setProperty("com.gs.disable.hybrid","true");
+        super.setUp();
+    }
 
     public void testNull() {
         // Arrange:
