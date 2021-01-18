0b60a1f0f3e5e7d819c9756011bf3f0c7b476551
Sagiv Michael <sagiv.michael@gigaspaces.com>
2021-01-18 16:46:45 +0200

    GS-14394 changes the cli timeout exception handle for Deploying/undeploying a space to show request status info instead of ERROR message.

--- xap-tools/xap-cli/src/main/java/org/gigaspaces/cli/CliCommandException.java
+++ xap-tools/xap-cli/src/main/java/org/gigaspaces/cli/CliCommandException.java
@@ -3,6 +3,7 @@ package org.gigaspaces.cli;
 public class CliCommandException extends Exception {
     public static final int CODE_GENERAL_ERROR = 1;
     public static final int CODE_INVALID_INPUT = 2;
+    public static final int CODE_TIMEOUT = 6;
 
     private int exitCode = CODE_GENERAL_ERROR;
 
@@ -29,5 +30,9 @@ public class CliCommandException extends Exception {
 
     public boolean isUserError() {
         return exitCode == CODE_INVALID_INPUT;
+    }
+
+    public boolean isTimeoutError() {
+        return exitCode == CODE_TIMEOUT;
     }
 }
