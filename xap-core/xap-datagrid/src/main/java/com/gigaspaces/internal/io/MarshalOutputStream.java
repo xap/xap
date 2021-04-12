780ff58bb3b89b151673189e4e2557b3bca1bb7e
nivin <niv@gigaspaces.com>
2021-04-12 15:34:21 +0300

    Reduced noisy log to debug

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/io/MarshalOutputStream.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/io/MarshalOutputStream.java
@@ -194,7 +194,7 @@ public class MarshalOutputStream
         int i = _context.getObjectStreamClassKey(desc);
         boolean newClass = false;
         if (i == 0 || !_optimize) {
-            _logger.info("new class: {} (size: {})", desc.getName(), _nextClassId);
+            _logger.debug("new class: {} (size: {})", desc.getName(), _nextClassId);
             newClass = true;
             if (_optimize) {
                 i = _nextClassId++;
