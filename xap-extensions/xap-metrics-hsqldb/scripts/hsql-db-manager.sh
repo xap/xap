07a9da148ff908e7246d1a26fc335aeab3b57f68
Evgeny Fisher <evgenyf@gmail.com>
2021-04-21 15:48:28 +0300

    upgrade hsqldb version from 2.3.2 to 2.5.2

--- xap-extensions/xap-metrics-hsqldb/scripts/hsql-db-manager.sh
+++ xap-extensions/xap-metrics-hsqldb/scripts/hsql-db-manager.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -cp ../lib/optional/jdbc/hsqldb-2.3.2.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost:9101/metricsdb
+java -cp ../lib/optional/jdbc/hsqldb-2.5.2.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost:9101/metricsdb
