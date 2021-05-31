79d909ee0ccd32d6de476bc68105692af47ae2de
yohanakh <yohana.khoury@gmail.com>
2021-05-31 14:12:15 +0300

    GS-14516 initial support for pg_catalog with hardcoded values

--- xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/calcite/GSTable.java
+++ xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/calcite/GSTable.java
@@ -13,8 +13,10 @@ import java.math.BigDecimal;
 public class GSTable extends AbstractTable {
 
     private final ITypeDesc typeDesc;
+    private final String name;
 
-    public GSTable(ITypeDesc typeDesc) {
+    public GSTable(String name, ITypeDesc typeDesc) {
+        this.name = name;
         this.typeDesc = typeDesc;
     }
 
@@ -70,6 +72,7 @@ public class GSTable extends AbstractTable {
             return SqlTypeName.ANY;
         }
 
+
         throw new UnsupportedOperationException("Unsupported type: " + clazz);
     }
 
@@ -86,7 +89,7 @@ public class GSTable extends AbstractTable {
         return builder.build();
     }
 
-    public ITypeDesc getTypeDesc() {
-        return typeDesc;
+    public String getName() {
+        return name;
     }
 }
