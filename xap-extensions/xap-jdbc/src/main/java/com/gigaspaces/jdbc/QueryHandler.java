ecede21dfc3ac35848ba066f4a96909c8d684238
Evgeny Fisher <evgenyf@gmail.com>
2021-06-01 10:08:12 +0300

    GS-14512 initial commit for group by support, tests added, added suport for groupby in QueryHandler by adding Feature.selectGroupBy to list of allowedFeatures

--- xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/QueryHandler.java
+++ xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/QueryHandler.java
@@ -24,7 +24,7 @@ import java.util.Set;
 
 public class QueryHandler {
 
-    private final Feature[] allowedFeatures = new Feature[] {Feature.select, Feature.explain, Feature.exprLike, Feature.jdbcParameter, Feature.join, Feature.joinInner, Feature.joinLeft};
+    private final Feature[] allowedFeatures = new Feature[] {Feature.select, Feature.explain, Feature.exprLike, Feature.jdbcParameter, Feature.join, Feature.joinInner, Feature.joinLeft, Feature.selectGroupBy};
 
     public ResponsePacket handle(String query, IJSpace space, Object[] preparedValues) throws SQLException {
 
