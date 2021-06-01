49210388ca2ee33b7f947488ebccab8e983af503
nivin <niv@gigaspaces.com>
2021-06-01 17:25:16 +0300

    GS-14508 updated javadoc, removed @ExperimentalApi

--- xap-core/xap-common/src/main/java/com/gigaspaces/serialization/SmartExternalizable.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/serialization/SmartExternalizable.java
@@ -1,18 +1,30 @@
 package com.gigaspaces.serialization;
 
-import com.gigaspaces.api.ExperimentalApi;
-
 import java.io.Externalizable;
 
 /**
- * Externalizable markup extension for GigaSpaces serialization - when an instance
- * of this interface is serialized, GigaSpaces generates a factory class to instantiate
- * it using the default constructor, thereby speeding up serialization significantly.
- * The relevant GigaSpaces serializable classes extend this interface.
+ * Externalizable markup extension for GigaSpaces enhanced serialization.
+ *
+ * When an instance of this interface is serialized using GigaSpaces, the following optimizations take place:
+ * 1. The first time it is serialized on a channel, it's assigned an id (int), which is cached on the receiving side of
+ * the channel. Subsequent serializations write the id instead of the class descriptor, which reduce cpu and network usage.
+ * 2. The first time it is deserialized on a channel, GigaSpaces generates a factory class (using byte-code generation)
+ * which creates instances of the class using the default constructor. This factory is used to instantiate deserialized
+ * instances instead of reflection, thus reducing cpu usage significantly.
+ *
+ * This interface is in use by relevant GigaSpaces serializable classes.
+ * Users may use this interface on their classes as well. However, keep in mind this enhancement intentionally avoids the
+ * standard writeObject/readObject mechanism for performance, including its reference identity mechanism:
+ * a) If an instance implementing SmartExternalizable is serialized multiple times in the same operation, it will be
+ * serialized separately each time, leading to extra resource usage and possibly unexpected behaviour if the receiving
+ * end expects references identity to be preserved.
+ * b) If an instance implementing SmartExternalizable is part of a references cycle, the serializing thread will run
+ * into an infinite loop and hang.
+ * For performance reasons, the SmartExternalizable does not handle those cases - it is the user's responsibility to
+ * check if they are relevant for their application when deciding whether or not to use SmartExternalizable
  *
  * @author Niv Ingberg
  * @since 16.0
  */
-@ExperimentalApi
 public interface SmartExternalizable extends Externalizable {
 }
