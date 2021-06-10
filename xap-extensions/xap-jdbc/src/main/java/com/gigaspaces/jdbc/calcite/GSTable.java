eb98b927ac9c625e5ad2839ef6501f1feb87209d
mavigdor <moran@gigaspaces.com>
2021-06-10 11:44:58 +0300

    GS-14524 Add tests package for calcite coverage

--- xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/calcite/GSTable.java
+++ xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/calcite/GSTable.java
@@ -28,13 +28,23 @@ public class GSTable extends AbstractTable {
     }
 
     private static SqlTypeName mapToSqlType(Class<?> clazz) {
-        if (clazz == Integer.class) {
+        if (clazz == Short.class) {
+            return SqlTypeName.SMALLINT;
+        } else if (clazz == Integer.class) {
             return SqlTypeName.INTEGER;
         } else if (clazz == Long.class) {
             return SqlTypeName.BIGINT;
+        } else if (clazz == Double.class) {
+            return SqlTypeName.DOUBLE;
+        } else if (clazz == Float.class) {
+            return SqlTypeName.REAL;
         } else if (clazz == String.class) {
             return SqlTypeName.VARCHAR;
+        } else if (clazz == Boolean.class) {
+            return SqlTypeName.BOOLEAN;
         } else if (clazz == java.util.Date.class) {
+            return SqlTypeName.DATE;
+        } else if (clazz == java.sql.Date.class) {
             return SqlTypeName.DATE;
         } else if (clazz == java.sql.Time.class) {
             return SqlTypeName.TIME;
