88dff204e68906cd4b96582472a73b90b2683686
mavigdor <moran@gigaspaces.com>
2021-06-13 15:42:02 +0300

    GS-14524 Fix exception handling - all tests pass - after rebase

--- xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/QueryHandler.java
+++ xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/QueryHandler.java
@@ -3,12 +3,15 @@ package com.gigaspaces.jdbc;
 import com.gigaspaces.jdbc.calcite.CalciteRootVisitor;
 import com.gigaspaces.jdbc.calcite.GSOptimizer;
 import com.gigaspaces.jdbc.calcite.GSRelNode;
+import com.gigaspaces.jdbc.exceptions.GenericJdbcException;
 import com.gigaspaces.jdbc.exceptions.SQLExceptionWrapper;
 import com.gigaspaces.jdbc.jsql.JsqlPhysicalPlanHandler;
 import com.gigaspaces.jdbc.model.QueryExecutionConfig;
 import com.gigaspaces.jdbc.model.result.QueryResult;
 import com.j_spaces.core.IJSpace;
 import com.j_spaces.jdbc.ResponsePacket;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.parser.feature.Feature;
 import net.sf.jsqlparser.statement.ExplainStatement;
 import net.sf.jsqlparser.statement.Statement;
@@ -30,6 +33,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.sql.SQLException;
 import java.util.Collections;
+import java.util.Properties;
 import java.util.Set;
 
 public class QueryHandler {
@@ -37,20 +41,23 @@ public class QueryHandler {
     private final Feature[] allowedFeatures = new Feature[] {Feature.select, Feature.explain, Feature.exprLike, Feature.jdbcParameter, Feature.join, Feature.joinInner, Feature.joinLeft};
 
     public ResponsePacket handle(String query, IJSpace space, Object[] preparedValues) throws SQLException {
-        GSRelNode calcitePlan = optimizeWithCalcite(query, space);
-        return executeStatement(space, calcitePlan, preparedValues);
-        // TODO create a flag to toggle between calcite/jsql
-//        try {
-//            Statement statement = CCJSqlParserUtil.parse(query);
-//            validateStatement(statement);
-//            return executeStatement(space, statement, preparedValues);
-//        } catch (JSQLParserException e) {
-//            throw new SQLException("Failed to parse query", e);
-//        } catch (SQLExceptionWrapper e) {
-//            throw e.getException();
-//        } catch (GenericJdbcException | UnsupportedOperationException e) {
-//            throw new SQLException(e.getMessage(), e);
-//        }
+        Properties customProperties = space.getURL().getCustomProperties();
+        if ("calcite".equals(customProperties.getProperty("v3driver"))) {
+            GSRelNode calcitePlan = optimizeWithCalcite(query, space);
+            return executeStatement(space, calcitePlan, preparedValues);
+        }
+        //else jsql
+        try {
+            Statement statement = CCJSqlParserUtil.parse(query);
+            validateStatement(statement);
+            return executeStatement(space, statement, preparedValues);
+        } catch (JSQLParserException e) {
+            throw new SQLException("Failed to parse query", e);
+        } catch (SQLExceptionWrapper e) {
+            throw e.getException();
+        } catch (GenericJdbcException | UnsupportedOperationException e) {
+            throw new SQLException(e.getMessage(), e);
+        }
     }
 
     private ResponsePacket  executeStatement(IJSpace space, GSRelNode relNode, Object[] preparedValues) throws SQLException {
