00a385af8fcc6c7644333f943b01409529408f6a
alonshoham <alon.shoham@gmail.com>
2021-06-22 08:41:19 +0300

    GS-14524 join improvement

--- xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/QueryExecutor.java
+++ xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/QueryExecutor.java
@@ -1,25 +1,16 @@
 package com.gigaspaces.jdbc;
 
-import com.gigaspaces.jdbc.exceptions.SQLExceptionWrapper;
-import com.gigaspaces.jdbc.handlers.*;
 import com.gigaspaces.jdbc.model.QueryExecutionConfig;
-import com.gigaspaces.jdbc.model.join.JoinInfo;
 import com.gigaspaces.jdbc.model.result.QueryResult;
-import com.gigaspaces.jdbc.model.table.*;
+import com.gigaspaces.jdbc.model.table.AggregationColumn;
+import com.gigaspaces.jdbc.model.table.IQueryColumn;
+import com.gigaspaces.jdbc.model.table.TableContainer;
 import com.j_spaces.core.IJSpace;
-import com.j_spaces.jdbc.builder.QueryTemplatePacket;
-import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
-import net.sf.jsqlparser.schema.Column;
-import net.sf.jsqlparser.schema.Table;
-import net.sf.jsqlparser.statement.select.*;
 
 import java.sql.SQLException;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class QueryExecutor extends SelectVisitorAdapter implements FromItemVisitor {
+public class QueryExecutor {
     private final List<TableContainer> tables = new ArrayList<>();
     private final Set<IQueryColumn> invisibleColumns = new HashSet<>();
     private final List<IQueryColumn> visibleColumns = new ArrayList<>();
@@ -28,6 +19,7 @@ public class QueryExecutor extends SelectVisitorAdapter implements FromItemVisit
     private final QueryExecutionConfig config;
     private final Object[] preparedValues;
     private boolean isAllColumnsSelected = false;
+    private final LinkedList<Integer> fieldCountList = new LinkedList<>();
 
 
     public QueryExecutor(IJSpace space, QueryExecutionConfig config, Object[] preparedValues) {
@@ -40,162 +32,13 @@ public class QueryExecutor extends SelectVisitorAdapter implements FromItemVisit
         this(space, new QueryExecutionConfig(), preparedValues);
     }
 
-    @Override
-    public void visit(PlainSelect plainSelect) {
-        plainSelect.getFromItem().accept(this);
-        if (plainSelect.getJoins() != null) {
-            plainSelect.getJoins().forEach(this::handleJoin);
-        }
-        prepareQueryColumns(plainSelect);
-        prepareWhereClause(plainSelect);
-        prepareOrderByClause(plainSelect);
-        prepareGroupByClause(plainSelect);
-        prepareDistinctClause(plainSelect);
-    }
-
-    private void handleJoin(Join join){
-        Expression onExpression = join.getOnExpression();
-        if(onExpression instanceof EqualsTo) {
-            if (! (join.getRightItem() instanceof Table)) {
-                throw new UnsupportedOperationException("Join is currently supported with concrete tables only");
-            }
-            Table rTable = (Table) join.getRightItem();
-            tables.add(createTableContainer(rTable));
-            Column rColumn = (Column) ((EqualsTo) onExpression).getRightExpression();
-            Column lColumn = (Column) ((EqualsTo) onExpression).getLeftExpression();
-            if(!rColumn.getTable().getName().equals(rTable.getAlias().getName())){ // in case on condition columns are reversed A JOIN B ON B.id = A.id
-                Column tmp = rColumn;
-                rColumn = lColumn;
-                lColumn = tmp;
-            }
-            TableContainer rightTable = QueryColumnHandler.getTableForColumn(rColumn, tables);
-            TableContainer leftTable = QueryColumnHandler.getTableForColumn(lColumn, tables);
-            IQueryColumn rightColumn = rightTable.addQueryColumn(rColumn.getColumnName(), null, false, -1);
-            IQueryColumn leftColumn = leftTable.addQueryColumn(lColumn.getColumnName(), null, false, -1);
-            rightTable.setJoinInfo(new JoinInfo(leftColumn, rightColumn, JoinInfo.JoinType.getType(join)));
-            if (leftTable.getJoinedTable() == null) { // TODO set right table every time and align it to recursive form in JoinTablesIterator
-                if (!rightTable.isJoined()) {
-                    leftTable.setJoinedTable(rightTable);
-                    rightTable.setJoined(true);
-                }
-            }
-        }
-        else {
-            throw new UnsupportedOperationException("Only simple ON clause is supported");
-        }
-    }
-
-    private void prepareQueryColumns(PlainSelect plainSelect) {
-        QueryColumnHandler queryColumnHandler = new QueryColumnHandler(this);
-        plainSelect.getSelectItems().forEach(selectItem -> selectItem.accept(queryColumnHandler));
-    }
-
-    private void prepareWhereClause(PlainSelect plainSelect) {
-        if (plainSelect.getWhere() != null) {
-            WhereHandler expressionVisitor = new WhereHandler(this.getTables(), preparedValues);
-            plainSelect.getWhere().accept(expressionVisitor);
-            for (Map.Entry<TableContainer, QueryTemplatePacket> tableContainerQueryTemplatePacketEntry : expressionVisitor.getQTPMap().entrySet()) {
-                tableContainerQueryTemplatePacketEntry.getKey().setQueryTemplatePacket(tableContainerQueryTemplatePacketEntry.getValue());
-            }
-
-            for (Map.Entry<TableContainer, Expression> tableContainerExpressionEntry : expressionVisitor.getExpTree().entrySet()) {
-                tableContainerExpressionEntry.getKey().setExpTree(tableContainerExpressionEntry.getValue());
-            }
-        }
-    }
-
-    private void prepareOrderByClause(PlainSelect plainSelect) {
-        if (plainSelect.getOrderByElements() != null) {
-            OrderByHandler orderByVisitor = new OrderByHandler(this);
-            plainSelect.getOrderByElements().forEach(orderByElement -> orderByElement.accept(orderByVisitor));
-        }
-    }
-
-    private void prepareGroupByClause(PlainSelect plainSelect) {
-        if (plainSelect.getGroupBy() != null) {
-            GroupByHandler groupByVisitor = new GroupByHandler(this);
-            plainSelect.getGroupBy().accept( groupByVisitor );
-        }
-    }
-
-    private void prepareDistinctClause(PlainSelect plainSelect) {
-
-        if (plainSelect.getDistinct() != null) {
-            List<SelectItem> items = plainSelect.getSelectItems();
-            if (items.get(0) instanceof AllColumns){
-                for (TableContainer table : tables){
-                    if(!table.getVisibleColumns().isEmpty())
-                        table.setDistinct(true);
-                }
-            }
-            else {
-                for (SelectItem item : items) {
-                    SelectExpressionItem sItem = (SelectExpressionItem) item;
-                    QueryColumnHandler.getTableForColumn((Column) sItem.getExpression(), tables).setDistinct(true);
-                }
-            }
-        }
-    }
-
-    @Override
-    public void visit(Table table) {
-        tables.add(createTableContainer(table));
-    }
-
-    private TableContainer createTableContainer(Table table){
-        return new ConcreteTableContainer(table.getFullyQualifiedName(), table.getAlias() == null ? null : table.getAlias().getName(), space);
-    }
-
-    @Override
-    public void visit(SubSelect subSelect) {
-        QueryExecutor subQueryExecutor = new QueryExecutor(space, config, preparedValues);
-        try {
-            tables.add(new TempTableContainer(subQueryExecutor.execute(subSelect.getSelectBody()), subSelect.getAlias() == null ? null : subSelect.getAlias().getName()));
-        } catch (SQLException e) {
-            throw new SQLExceptionWrapper(e);
-        }
-
-    }
-
-    @Override
-    public void visit(SubJoin subjoin) {
-        throw new UnsupportedOperationException("Unsupported yet!");
-    }
-
-    @Override
-    public void visit(LateralSubSelect lateralSubSelect) {
-        throw new UnsupportedOperationException("Unsupported yet!");
-    }
-
-    @Override
-    public void visit(ValuesList valuesList) {
-        throw new UnsupportedOperationException("Unsupported yet!");
-    }
-
-    @Override
-    public void visit(TableFunction tableFunction) {
-        throw new UnsupportedOperationException("Unsupported yet!");
-    }
-
-    @Override
-    public void visit(ParenthesisFromItem aThis) {
-        throw new UnsupportedOperationException("Unsupported yet!");
-    }
-
-    public QueryResult execute(SelectBody selectBody) throws SQLException {
-        prepareForExecution(selectBody);
-
+    public QueryResult execute() throws SQLException {
         if (tables.size() == 1) { //Simple Query
             return tables.get(0).executeRead(config);
         }
         JoinQueryExecutor joinE = new JoinQueryExecutor(this);
         return joinE.execute();
     }
-
-    private void prepareForExecution(SelectBody selectBody) {
-        selectBody.accept(this);
-    }
-
 
     public List<TableContainer> getTables() {
         return tables;
@@ -245,7 +88,33 @@ public class QueryExecutor extends SelectVisitorAdapter implements FromItemVisit
         this.aggregationColumns.add(aggregationColumn);
     }
 
-    public List<IQueryColumn> getSelectedColumns() {
-        return Stream.concat(getVisibleColumns().stream(), getAggregationColumns().stream()).sorted().collect(Collectors.toList());
+    public void addTable(TableContainer tableContainer){
+        tables.add(tableContainer);
+        addFieldCount(tableContainer.getVisibleColumns().size());
+    }
+
+    public TableContainer getTableByColumnIndex(int columnIndex){
+        for (int i = 0; i < fieldCountList.size(); i++) {
+            if(columnIndex < fieldCountList.get(i)){
+                return getTables().get(i);
+            }
+        }
+        throw new UnsupportedOperationException("");
+    }
+
+    public IQueryColumn getColumnByColumnIndex(int globalColumnIndex){
+        for (int i = 0; i < fieldCountList.size(); i++) {
+            if(globalColumnIndex < fieldCountList.get(i)){
+                int columnIndex = i == 0 ? globalColumnIndex : globalColumnIndex - fieldCountList.get(i - 1);
+                return getTables().get(i).getVisibleColumns().get(columnIndex);
+            }
+        }
+        throw new UnsupportedOperationException("");
+    }
+
+
+    public void addFieldCount(int size) {
+        int columnCount = fieldCountList.isEmpty() ?  size: fieldCountList.getLast() + size;
+        fieldCountList.add(columnCount);
     }
 }
