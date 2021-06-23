668e38c511055541de7130a964e8bbcea1c28d6e
Noi Cohen <noicoh@gmail.com>
2021-06-23 13:48:41 +0300

    add SerialVersionUID to classes

--- xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/exceptions/SQLExceptionWrapper.java
+++ xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/exceptions/SQLExceptionWrapper.java
@@ -3,6 +3,7 @@ package com.gigaspaces.jdbc.exceptions;
 import java.sql.SQLException;
 
 public class SQLExceptionWrapper extends GenericJdbcException {
+    static final long serialVersionUID = -1247755779483927872L;
     private final SQLException e;
 
     public SQLExceptionWrapper(SQLException e) {
