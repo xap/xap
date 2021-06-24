0c45911971054cde555deb747221ff825d2db844
Tomer Shapira <tomer.shapira@gigaspaces.com>
2021-06-24 16:36:07 +0300

    GS-14526 fix test

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/SpaceEngine.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/SpaceEngine.java
@@ -104,6 +104,7 @@ import com.gigaspaces.lrmi.nio.IResponseContext;
 import com.gigaspaces.lrmi.nio.ResponseContext;
 import com.gigaspaces.management.space.SpaceQueryDetails;
 import com.gigaspaces.metrics.*;
+import com.gigaspaces.query.aggregators.DistinctAggregator;
 import com.gigaspaces.query.aggregators.OrderByAggregator;
 import com.gigaspaces.query.aggregators.SpaceEntriesAggregator;
 import com.gigaspaces.security.authorities.SpaceAuthority.SpacePrivilege;
@@ -6502,6 +6503,11 @@ public class SpaceEngine implements ISpaceModeListener , IClusterInfoChangedList
                           SpaceContext sc)
             throws Exception {
         if (Modifiers.contains(readModifiers, Modifiers.EXPLAIN_PLAN)) {
+            // for now support only orderBy
+            if(!aggregators.stream().allMatch(spaceEntriesAggregator -> spaceEntriesAggregator instanceof OrderByAggregator
+                    || (spaceEntriesAggregator instanceof DistinctAggregator && !((DistinctAggregator) spaceEntriesAggregator).isGroupByAggregator()))) {
+                throw new UnsupportedOperationException("Sql explain plan is only supported by OrderBy and Distinct aggregation");
+            }
             SingleExplainPlan.validate(0, _cacheManager.isBlobStoreCachePolicy(), readModifiers,
                     queryPacket.getCustomQuery(), getClassTypeInfo(queryPacket.getTypeName()).getIndexes());
         }
