88c35bf301f2e9739df99c262ac5d0f7eb0009fc
yohanakh <yohana.khoury@gmail.com>
2021-06-28 19:47:16 +0300

    GS-14503 fixed failing tests

--- xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/QueryHandler.java
+++ xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/QueryHandler.java
@@ -2,6 +2,8 @@ package com.gigaspaces.jdbc;
 
 import com.gigaspaces.jdbc.calcite.CalciteDefaults;
 import com.gigaspaces.jdbc.calcite.handlers.CalciteQueryHandler;
+import com.gigaspaces.jdbc.exceptions.GenericJdbcException;
+import com.gigaspaces.jdbc.exceptions.SQLExceptionWrapper;
 import com.gigaspaces.jdbc.jsql.handlers.JsqlQueryHandler;
 import com.j_spaces.core.IJSpace;
 import com.j_spaces.jdbc.ResponsePacket;
@@ -13,11 +15,17 @@ public class QueryHandler {
 
 
     public ResponsePacket handle(String query, IJSpace space, Object[] preparedValues) throws SQLException {
-        Properties customProperties = space.getURL().getCustomProperties();
-        if (CalciteDefaults.isCalciteDriverPropertySet(customProperties)) {
-            return new CalciteQueryHandler().handle(query, space, preparedValues);
-        } else { //else jsql
-            return new JsqlQueryHandler().handle(query, space, preparedValues);
+        try {
+            Properties customProperties = space.getURL().getCustomProperties();
+            if (CalciteDefaults.isCalciteDriverPropertySet(customProperties)) {
+                return new CalciteQueryHandler().handle(query, space, preparedValues);
+            } else { //else jsql
+                return new JsqlQueryHandler().handle(query, space, preparedValues);
+            }
+        } catch (SQLExceptionWrapper e) {
+            throw e.getException();
+        } catch (GenericJdbcException | UnsupportedOperationException e) {
+            throw new SQLException(e.getMessage(), e);
         }
     }
 
